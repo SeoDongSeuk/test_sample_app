077a3728b5da002bc84ab7d36afc04781ff5f408
SeoDongSeuk <cast3613@gmail.com>
2023-08-26 13:39:26 +0900

    test sample app first push

---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:counting_sample_app/Dto/dto_tenping_data.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(const MaterialApp(home: HomePage()));
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  var tenpingUri =
+      "http://tenping.kr/adbox/list?MemberID=jHyhVTbomtJpJAihOcAt13hRhjDEKHWTknCzHBM7NMAKkyVMQ6z4IZccaxhZNjtG&PageSize=30&CampaignType=0&MinClickPoint=0&MinCurrentPoint=50&ExistMiddleImage=&ExistRectangleImage=&IsPartner=";
+
+  Map<String, dynamic> data = {};
+  List listData = [];
+  List<TenpingListData> tenpingListData = [];
+
+  Future<String> getData() async {
+    var response = await http
+        .get(Uri.parse(tenpingUri), headers: {"Accept": "application/json"});
+
+    setState(() {
+      data = (json.decode(response.body));
+    });
+
+    listData = data["List"];
+
+    tenpingListData = listData
+        .map(
+          (element) => TenpingListData.fromJson(element),
+        )
+        .toList();
+
+    return "Success!";
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Listviews"),
+        backgroundColor: Colors.blue,
+      ),
+      body: ListView.builder(
+        itemCount: tenpingListData == null ? 0 : listData.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            child: GestureDetector(
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Image(
+                        image:
+                            //NetworkImage(listData[index]["SImage"].toString()),
+                            NetworkImage(
+                                tenpingListData[index].sImage.toString()),
+                        width: 100,
+                        height: 100,
+                        fit: BoxFit.fill,
+                      ),
+                      Column(
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                            width: 260,
+                            child: Text(
+                              listData[index]["ContentTitle"].toString(),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              onTap: () async {
+                final url = Uri.parse(
+                  listData[index]["Link"],
+                );
+                if (await canLaunchUrl(url)) {
+                  launchUrl(url);
+                } else {}
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
